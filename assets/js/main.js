e8213d1ec8de3c2aa514c87be408c2a900550d38
Juan Sánchez <juansmjeje@gmail.com>
2023-07-30 20:59:39 +0200

    responsive problems

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,3 +12,46 @@ AOS.init({
   mirror: false, // whether elements should animate out while scrolling past them
   anchorPlacement: "top-bottom", // defines which position of the element regarding to window should trigger the animation
 });
+
+// Función para mostrar solo los skill-icons relacionados con una skill específica
+function showSkillIcons(skillClass) {
+  const allSkillIcons = document.querySelectorAll(".skill-icon");
+  allSkillIcons.forEach((icon) => {
+    if (icon.classList.contains(skillClass)) {
+      icon.style.display = "block";
+    } else {
+      icon.style.display = "none";
+    }
+  });
+}
+
+// Función para mostrar todos los skill-icons
+function showAllSkillIcons() {
+  const allSkillIcons = document.querySelectorAll(".skill-icon");
+  allSkillIcons.forEach((icon) => {
+    icon.style.display = "block";
+  });
+}
+
+// Asigna los eventos mouseover y mouseleave a cada skill
+document.addEventListener("DOMContentLoaded", () => {
+  const webDeveloperSkill = document.querySelector(".web-developer");
+  const dbaDataAnalystSkill = document.querySelector(".dba-data-analyst");
+
+  webDeveloperSkill.addEventListener("mouseover", () => {
+    showSkillIcons("web-developer");
+  });
+
+  dbaDataAnalystSkill.addEventListener("mouseover", () => {
+    showSkillIcons("dba-data-analyst");
+  });
+
+  // Evento mouseleave para restablecer todas las skill-icons
+
+  webDeveloperSkill.addEventListener("mouseleave", () => {
+    showAllSkillIcons();
+  });
+  dbaDataAnalystSkill.addEventListener("mouseleave", () => {
+    showAllSkillIcons();
+  });
+});
