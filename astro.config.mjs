7d090794be7f6cff5002dcd66839ec47f7d29af3
juansm01 <juanschezmor@gmail.com>
2024-07-10 19:44:57 +0200

    deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://juanschezmor.dev',
   integrations: [icon(), react()],
   css: {
     global: 'src/styles/global.css',
